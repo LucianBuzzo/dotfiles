89c26da08ac7bd989e74d91af62ad5e69577fb56
Lucian Buzzo <lucian.buzzo@gmail.com>
2018-03-04 22:24:48 +0000

    Call setup script with bash

--- oni.config.js
+++ oni.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  "oni.useDefaultConfig": true,
-  "oni.statusbar.enabled": true,
   "oni.loadInitVim": true,
+  "oni.useDefaultConfig": true,
   "ui.colorscheme": "solarized8_light",
 
   "colors.background": "#eee8d5",
   "colors.title.background": "#eee8d5",
+  "toolTip.background": "#eee8d5",
 
   "editor.fontFamily": "Source Code Pro for Powerline",
   "editor.fontSize": 12,
   "editor.completions.enabled": true,
   "editor.formatting.formatOnSwitchToNormalMode": true,
   "autoClosingPairs.enabled": false,
-  "sidebar.enabled": false,
+  "sidebar.enabled": true,
 }
