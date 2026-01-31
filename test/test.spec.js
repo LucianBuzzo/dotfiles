df6539f9ad4a8dbc7a7629e20b301447df4c889f
Lucian Buzzo <lucian.buzzo@gmail.com>
2026-01-31 09:01:00 +0000

    refactor(tests): migrate from mocha/chai to jest

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,4 +1,3 @@
-const { expect } = require('chai')
 const { execSync } = require('child_process')
 const fs = require('fs')
 const { homedir } = require('os')
@@ -13,46 +12,43 @@ describe('setup.sh', () => {
   it('should create a symlink for .vimrc in the home directory', () => {
     const linkPath = path.join(HOME, '.vimrc')
     const stats = fs.lstatSync(linkPath)
-    expect(!!stats && stats.isSymbolicLink()).to.be.true
+    expect(!!stats && stats.isSymbolicLink()).toBe(true)
 
     const linkTargetPath = fs.readlinkSync(linkPath)
-    expect(linkTargetPath).to.equal(path.join(CWD, 'vim', '.vimrc'))
+    expect(linkTargetPath).toBe(path.join(CWD, 'vim', '.vimrc'))
   })
 
-  it('should create a symlink for .vimbackups/ in the home directory', () => {
-    const linkPath = path.join(HOME, '.vimbackups')
-    const stats = fs.lstatSync(linkPath)
-    expect(!!stats && stats.isSymbolicLink()).to.be.true
-
-    const linkTargetPath = fs.readlinkSync(linkPath)
-    expect(linkTargetPath).to.equal(path.join(CWD, 'vim', '.vimbackups/'))
+  it('should create the .vimbackups/ directory in the home directory', () => {
+    const dirPath = path.join(HOME, '.vimbackups')
+    const stats = fs.lstatSync(dirPath)
+    expect(stats.isDirectory()).toBe(true)
   })
 
   it('should create a symlink for vim/ in the home directory', () => {
     const linkPath = path.join(HOME, '.config', 'nvim')
     const stats = fs.lstatSync(linkPath)
-    expect(!!stats && stats.isSymbolicLink()).to.be.true
+    expect(!!stats && stats.isSymbolicLink()).toBe(true)
 
     const linkTargetPath = fs.readlinkSync(linkPath)
-    expect(linkTargetPath).to.equal(path.join(CWD, 'vim'))
+    expect(linkTargetPath).toBe(path.join(CWD, 'vim'))
   })
 
   it('should create a symlink for .inputrc in the home directory', () => {
     const linkPath = path.join(HOME, '.inputrc')
     const stats = fs.lstatSync(linkPath)
-    expect(!!stats && stats.isSymbolicLink()).to.be.true
+    expect(!!stats && stats.isSymbolicLink()).toBe(true)
 
     const linkTargetPath = fs.readlinkSync(linkPath)
-    expect(linkTargetPath).to.equal(path.join(CWD, '.inputrc'))
+    expect(linkTargetPath).toBe(path.join(CWD, '.inputrc'))
   })
 
   it('should create a symlink for .bash_profile in the home directory', () => {
     const linkPath = path.join(HOME, '.bash_profile')
     const stats = fs.lstatSync(linkPath)
-    expect(!!stats && stats.isSymbolicLink()).to.be.true
+    expect(!!stats && stats.isSymbolicLink()).toBe(true)
 
     const linkTargetPath = fs.readlinkSync(linkPath)
-    expect(linkTargetPath).to.equal(path.join(CWD, 'bash', '.bash_profile'))
+    expect(linkTargetPath).toBe(path.join(CWD, 'bash', '.bash_profile'))
   })
 
 })
